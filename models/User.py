85cb31e628cfd082c5e4f299e8c014d973a1fb17
Thiago3011 <thiago.silva1001@outlook.com>
2026-06-09 20:04:42 -0300

    feat: add UserUpdate class to update users

--- models/User.py
+++ models/User.py
@@ -9,3 +9,11 @@ class User(BaseModel):
     password: str
     status: bool = True
     creation_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    update_date: datetime | None = None
+
+class UserUpdate(BaseModel):
+    name: str | None = None
+    email: EmailStr | None = None
+    phone: str | None = None
+    password: str | None = None
+    status: bool | None = None
