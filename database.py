04d256ef94cd9f64df4c17a3dcd5d481db970e69
Thiago3011 <thiago.silva1001@outlook.com>
2026-06-15 20:12:12 -0300

    feat: add sqlalchemy to the project, creation of table users in test.db and configuring in database.py.

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
-users = []
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from routes import user_routes, login_routes
+# from routes import user_routes, login_routes
+from database import engine, Base
+from models.db.user import User
 
 app = FastAPI()
 
-app.include_router(user_routes.router)
-app.include_router(login_routes.router)
+Base.metadata.create_all(bind=engine)
+
+# app.include_router(user_routes.router)
+# app.include_router(login_routes.router)
 
 @app.get("/")
 def home():
