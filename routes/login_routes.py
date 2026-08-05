14876d1988b69d492bea4eede9003396fad98f2a
Thiago3011 <thiago.silva1001@outlook.com>
2026-08-05 19:34:08 -0300

    refactor: inject database session into authentication routes

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -1,7 +1,11 @@
 from fastapi.routing import APIRouter
 from fastapi import Depends
-from models.LoginRequest import LoginRequest
+from sqlalchemy.orm import Session
+from models.schemas.LoginRequest import LoginRequest
 from services import auth_services, token_handler
+from database import get_db
+from services.token_handler import oauth2_scheme
+
 
 router = APIRouter(
     prefix="/auth",
@@ -9,9 +13,9 @@ router = APIRouter(
 )
 
 @router.post("/login")
-def login(login_data: LoginRequest):
-    return auth_services.login(login_data)
+def login(login_data: LoginRequest, db: Session = Depends(get_db)):
+    return auth_services.login(login_data, db)
 
 @router.get("/me")
-def get_me(current_user: str = Depends(token_handler.get_user_with_token)):
-    return current_user
+def get_me(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    return token_handler.get_user_with_token(db, token)
