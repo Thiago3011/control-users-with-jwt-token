6ebc20b1e074885a137d4f8a393e928ee4b3d912
Thiago3011 <thiago.silva1001@outlook.com>
2026-08-05 19:34:25 -0300

    refactor: simplify JWT token handling with database services

--- services/token_handler.py
+++ services/token_handler.py
@@ -1,15 +1,18 @@
-from jose import jwt, JWTError
-from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends, HTTPException
+from jose import jwt
+from fastapi import HTTPException
 from datetime import datetime, timezone, timedelta
-from database import users
+from sqlalchemy.orm import Session
+from services.formatter import format_user
+from services.user_finder import find_user
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "minha_chave"
 ALGORITHM = "HS256"
 
+# Dependency used by FastAPI to extract the JWT token
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-def create_access_token(user_id):
+def create_access_token(user_id: int) -> str:
     expiration_time = datetime.now(timezone.utc) + timedelta(seconds=40) 
     payload = {
         "user_id": user_id,
@@ -17,28 +20,14 @@ def create_access_token(user_id):
     }
     return jwt.encode(payload, SECRET_KEY, ALGORITHM)
 
-def decode_access_token(token: str = Depends(oauth2_scheme)):
+def decode_access_token(token: str) -> dict:
     return jwt.decode(token, SECRET_KEY, [ALGORITHM])
 
-def get_user_with_token(token: str = Depends(oauth2_scheme)):
-    try:
+def get_user_with_token(db: Session, token: str):
         decoded_token = decode_access_token(token)
         decoded_user_id = decoded_token["user_id"]
         
-        for user in users:
-            if user.id == decoded_user_id:
-                return {
-                    "id": user.id,
-                    "name": user.name,
-                    "email": user.email,
-                    "phone": user.phone,
-                    "status": user.status,
-                    "creation_date": user.creation_date,
-                    "update_date": user.update_date,
-                }
-    except JWTError:
-       raise HTTPException(detail="Invalid or expired token", status_code=401)
-    
-    raise HTTPException(detail="User not found", status_code=401)
-    
-    
+        user = find_user(decoded_user_id, db)
+            
+        return format_user(user)
+            
